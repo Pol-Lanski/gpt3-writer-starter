7d89b7ba40df4cdf61b19def021c8b64798378d8
Lanski <19168735+Pol-Lanski@users.noreply.github.com>
2022-12-13 21:48:37 +0300

    added textarea and changed titles

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,15 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Any questions about Dappnode?</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask them here</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="Start typing here"
+            className="prompt-box" />
         </div>
       </div>
       <div className="badge-container grow">

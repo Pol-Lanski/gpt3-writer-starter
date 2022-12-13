a4037e85e8f43b513a2a02634b1b05906a14c31d
Lanski <19168735+Pol-Lanski@users.noreply.github.com>
2022-12-13 22:45:01 +0300

    created output field and loading state in the generate button

--- pages/index.js
+++ pages/index.js
@@ -58,12 +58,27 @@ const Home = () => {
             onChange={onUserChangedText}
           />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={callGenerateEndpoint}>
+            <a 
+              className={isGenerating ? "generate-button loading" : "generate-button"}
+              onClick={callGenerateEndpoint}
+            >
               <div className="generate">
-                <p>Generate</p>
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
               </div>
             </a>
           </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
 
       </div>
